dd8d6a77283687c86ec46c4282a7acdba8ab5fb6
Victor Prieto Ruiz <ge38tog@mytum.de>
2021-05-17 19:57:51 +0200

    Removed test case used for demo purposes.

--- PDEFEMCode/pde_test.py
+++ PDEFEMCode/pde_test.py
@@ -40,6 +40,7 @@ class TestPDESolver(TestCase):
         np.testing.assert_almost_equal(error_L2,0,decimal=10)
         np.testing.assert_almost_equal(error_LInf,0,decimal=10)
 
+
     def test_PDE_solve_sines(self):
         # Constant function is supplied.
         # The solver is expected to return the same constant.
@@ -56,47 +57,6 @@ class TestPDESolver(TestCase):
         np.testing.assert_almost_equal(error_LInf,0, decimal=2)
         # 5e-4 error reported in test MATLAB code.
 
-### TO BE DELETED SOON, JUST FOR DEMONSTRATION PURPOSES
-class TestParabolicSolver(TestCase):
-    mesh,fn_space = pde_utils.setup_function_space(100)
-    LHS = staticmethod(pde_utils.elliptic_LHS_const)
-    RHS = staticmethod(pde_utils.elliptic_RHS)
-
-    u_trial = fc.TrialFunction(fn_space)
-    v_test = fc.TestFunction(fn_space)
-
-    LHS_arg = {'alpha':0.5}
-
-    def solve_obtain_error(self,RHS_fn,u_ref):
-        '''
-        helper function for test cases below.
-        '''
-        LHS_int, RHS_int = pde_utils.variational_formulation(
-            self.u_trial, self.v_test,
-            self.LHS,
-            self.RHS, RHS_fn,
-            LHS_args=self.LHS_arg
-        )
-        u_sol = pde_utils.solve_pde(self.fn_space, LHS_int, RHS_int)
-
-        error_L2 = pde_utils.error_L2(u_ref, u_sol)
-        error_LInf = pde_utils.error_LInf_piece_lin(u_ref, u_sol, self.mesh)
-        return error_L2,error_LInf
-
-    def test_PDE_solve_constant(self):
-        # Constant function is supplied.
-        # The solver is expected to return the same constant.
-
-        RHS_fn = fc.Constant(3.0)
-        u_ref = RHS_fn
-
-        error_L2, error_LInf = self.solve_obtain_error(RHS_fn, u_ref)
-        print('Constant function error_L2  =', error_L2)
-        print('Constant function error_Linf =', error_LInf)
-        np.testing.assert_almost_equal(error_L2,0,decimal=10)
-        np.testing.assert_almost_equal(error_LInf,0,decimal=10)
-
-##############################
 
 
 class TestPDEWrap(unittest.TestCase):
